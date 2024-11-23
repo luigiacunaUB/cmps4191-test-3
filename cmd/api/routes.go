78806a73cc82b07f211de3348c81648447b00a66
Luigi Acuna <2015112896@ub.edu.bz>
2024-11-23 11:46:20 -0500

    added base

---
+++ cmd/api/routes.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (a *applicationDependencies) routes() http.Handler {
+
+	//setup a new router
+	router := httprouter.New()
+
+	//errors
+	//404
+	router.NotFound = http.HandlerFunc(a.notFoundResponse)
+	//405
+	router.MethodNotAllowed = http.HandlerFunc(a.methodNotAllowedResponse)
+	//routes
+	//basic
+	router.HandlerFunc(http.MethodGet, "/", a.Index)                            //root page
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", a.healthCheckHandler) //healthcheck
+
+	return a.recoverPanic(a.rateLimit(router))
+}
